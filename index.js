d72a69ef30870febdf4a11d4209b85d0b8ba7175
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-15 20:52:30 -0700

    completed with assist from slack searches

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(array){
+    let winYear = array.find(element => element.result === "W");
+    if (winYear){
+        return winYear.year;
+    } else {
+        console.log("undefined")
+    }
+};
